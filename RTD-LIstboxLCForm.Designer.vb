a31adbd61a62a07a4c7e9ec21cd13c975145adc5
Lane Coleman <lane.coleman@live.com>
2020-10-14 08:25:16 -0600

    Add project files.

---
+++ RTD-LIstboxLCForm.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RTDListBox = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'RTDListBox
+        '
+        Me.RTDListBox.FormattingEnabled = True
+        Me.RTDListBox.ItemHeight = 25
+        Me.RTDListBox.Location = New System.Drawing.Point(245, 147)
+        Me.RTDListBox.Name = "RTDListBox"
+        Me.RTDListBox.Size = New System.Drawing.Size(1067, 579)
+        Me.RTDListBox.TabIndex = 0
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(245, 776)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(218, 111)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(662, 776)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(218, 111)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(1094, 776)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(218, 111)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1580, 949)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.RTDListBox)
+        Me.Name = "Form1"
+        Me.Text = "Roll of The Dice - LIst Box"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RTDListBox As ListBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ RTD-LIstboxLCForm.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Randomize()
+        Dim randomNumber As Integer
+        Dim txt1, txt2, dashes As String
+        Dim data(10) As Integer
+
+
+        For i = 1 To 1000
+            randomNumber = CInt(GetRandomNumber(1, 6))
+            data(randomNumber - 2) += 1
+            'has to be randomnumber - 2 or it would be outside the bounds of the array.
+        Next
+
+        For i = 2 To 12
+            txt1 = txt1 & String.Format("{0, 11}", i) & "|"
+        Next
+
+        RTDListBox.Items.Add("Numbers:       " & txt1)
+        dashes = (StrDup(177, "-"))
+        RTDListBox.Items.Add(dashes)
+
+        For i = 0 To 10
+            txt2 = txt2 & String.Format("{0,10}", data(i)) & "|"
+        Next
+
+        RTDListBox.Items.Add("Times Rolled:" & txt2)
+        RTDListBox.Items.Add(dashes)
+        RTDListBox.Items.Add("")
+        RTDListBox.Items.Add("")
+    End Sub
+    Function GetRandomNumber(ByVal minimum As Single,
+                             ByVal maximum As Single) As Single
+
+        Dim value1, value2 As Double
+        Dim sum As Integer
+        'rolls two 'dice'  each gives a number from 0.5 to 6.5 and rounds to the nearest whole number
+        'they are then added together like real dice for a number between 2 and 12
+        Do
+            value1 = (maximum * Rnd()) + 0.5
+            value2 = (maximum * Rnd()) + 0.5
+        Loop While value1 < minimum - 0.5 Or value1 >= maximum + 0.5 Or
+            value2 < minimum - 0.5 Or value2 >= maximum + 0.5
+
+        sum = CInt(value1) + CInt(value2)
+        Return sum
+
+    End Function
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        RTDListBox.Items.Clear()
+    End Sub
+End Class
